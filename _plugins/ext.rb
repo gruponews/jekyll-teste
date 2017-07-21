10d0e7427905ab669af606734e70084838f2e2bc
Rubens Alencar <rubens.alencar@grupofolha.com.br>
2017-07-21 17:21:39 -0300

    update ext

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -2,9 +2,21 @@ Jekyll::Hooks.register :site, :post_read do |site|
   
   site.posts.docs.each do |x|
   	x.data['permalink'] = x.data['permalink'].strip
-  	# Jekyll.logger.info(x.data['permalink'])
   end
-  site.each do |x|
-  	Jekyll.logger.info(x)
+  # Jekyll.logger.info(site.collections['eventos'])
+  site.collections['eventos'].each do |x|
+  	x.data['permalink'] = x.data['permalink'].strip
+  end
+
+  site.collections['audiosevideos'].each do |x|
+  	x.data['permalink'] = x.data['permalink'].strip
+  	if x.data['wpcf-gn_audiovideo_audio'].is_a? Array
+	  	x.data['wpcf-gn_audiovideo_audio'].each do |url|
+				if url.include? 'http://www.gruponews.com.br/wp-content/'
+					url = url.gsub! 'http://www.gruponews.com.br/wp-content', '//assets.gruponews.com.br/gruponews'
+				end
+				Jekyll.logger.info(url)
+  		end
+  	end
   end
 end
