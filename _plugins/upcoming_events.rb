7320c79196941b74e0e6cff08a191e82cd7975f9
Rubens Alencar <rubens.alencar@grupofolha.com.br>
2017-08-04 17:35:20 -0300

    update

--- _plugins/upcoming_events.rb
+++ _plugins/upcoming_events.rb
@@ -6,20 +6,16 @@ module Reading
       site.collections.each do |name, collection|
         if name == 'eventos'
           site.data['upcomingEvents'] = collection.docs.select { |event|
+            # Jekyll.logger.info(event.data['title'])
 
             date = event.data['event']['event_start_date']
             time = (event.data['event']['event_start_time'] == 0.0 ? '00:00:00': event.data['event']['event_start_time'])
-            datetime = date.to_s + 'T' + time.to_s + '-03:00'
-            Jekyll.logger.info(date)
-            Jekyll.logger.info(time)
-            Jekyll.logger.info(Time.now)
-            Jekyll.logger.info( DateTime.strptime(datetime, '%Y-%m-%dT%H:%M:%S%z'))
-
-            DateTime.strptime(datetime, '%Y-%m-%dT%H:%M:%S%z') >= Time.now
+            dateUnix = date.to_s + 'T' + time.to_s + '-03:00'
+            datetime = Time.parse(dateUnix)
+            datetime.utc >= Time.now.utc
           }
         end
       end
     end
-
   end
 end
